399338c3b9fe3843775d00e66be42eecbf02d655
Harry Fox <1254033+hfoxy@users.noreply.github.com>
2023-07-26 13:23:01 +0100

    add enable iracing annotation

---
+++ src/main/java/me/hfox/iracing/data/api/EnableIRacing.java
@@ -0,0 +1,14 @@
+package me.hfox.iracing.data.api;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import(IRacingDataApiConfiguration.class)
+public @interface EnableIRacing {
+}
