995ab093c99142c94fcb4c27296e3d95601be398
Harry Fox <1254033+hfoxy@users.noreply.github.com>
2023-08-01 23:15:04 +0100

    update version number and remove ComponentScan

--- src/main/java/me/hfox/iracing/data/api/IRacingDataApiConfiguration.java
+++ src/main/java/me/hfox/iracing/data/api/IRacingDataApiConfiguration.java
@@ -1,9 +1,7 @@
 package me.hfox.iracing.data.api;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({"me.hfox.iracing.data.api.service"})
 public class IRacingDataApiConfiguration {
 }
